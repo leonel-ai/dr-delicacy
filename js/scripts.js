87748ac7caebcddf6333e12888127cac130b1dbf
Leonela Guzman <leonel-ai@users.noreply.github.com>
2018-05-09 15:12:46 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// JavaScript
+window.sr = ScrollReveal();
+sr.reveal('.home', { delay: 700, easing: 'ease', duration: 800, scale: 0 });
+sr.reveal('.products', { duration: 700 });
+sr.reveal('.story', { duration: 700 });
+sr.reveal('.contact', { duration: 700 });
+sr.reveal('.page-footer', { duration: 700 });
+
+$('.carousel').each(function () {
+  var $carousel = $(this);
+  var hammertime = new Hammer(this, {
+      recognizers: [
+          [Hammer.Swipe, { direction: Hammer.DIRECTION_HORIZONTAL }]
+      ]
+  });
+  hammertime.on('swipeleft', function () {
+      $carousel.carousel('next');
+  });
+  hammertime.on('swiperight', function () {
+      $carousel.carousel('prev');
+  });
+});
